88c2f51999fb2e8a0380afbc642d769656fefea9
Christopher Vollick <0@psycoti.ca>
2014-06-28 17:42:05 -0400

    Implement collapse_dirs
    This involved reimplementing filter_files into a function that was much more
    flexible.
    Happy.

--- Lister.hs
+++ Lister.hs
@@ -2,6 +2,8 @@ module Lister (
 	ListEntry (..),
 	build_list,
 	filter_files,
+	prune_dirs,
+	collapse_dirs,
 ) where
 
 import System.Directory (doesDirectoryExist, doesFileExist, getDirectoryContents)
@@ -26,6 +28,32 @@ build_list path = handleDir =<< doesDirectoryExist path
 	buildDirectory = fmap $ fmap (Directory path) . sequence
 	getActualDirs = fmap (filter filterDirs) $ getDirectoryContents path
 
+removeNothings :: [Maybe a] -> [a]
+removeNothings = foldr func []
+  where
+  func Nothing acc = acc
+  func (Just a) acc = a:acc
+
+-- This function applies a 
 filter_files :: (FilePath -> Bool) -> ListEntry -> Maybe ListEntry
-filter_files pred file@(File path) = if' (pred path) (Just file) Nothing
-filter_files pred (Directory path files) = fmap (Directory path) $ mconcat $ fmap (fmap pure . filter_files pred) files
+filter_files = post_process . func_filter_files 
+func_filter_files pred f@(File p) = if (pred p) then Just f else Nothing
+func_filter_files _ d = Just d
+
+prune_dirs :: ListEntry -> Maybe ListEntry
+prune_dirs = post_process func_prune_dirs
+func_prune_dirs f@(File _) = Just f
+func_prune_dirs (Directory _ []) = Nothing
+func_prune_dirs d = Just d 
+
+collapse_dirs :: ListEntry -> Maybe ListEntry
+collapse_dirs = post_process func_collapse_dirs
+func_collapse_dirs f@(File _) = Just f
+func_collapse_dirs (Directory _ [x]) = Just x
+func_collapse_dirs (Directory _ []) = Nothing
+func_collapse_dirs d = Just d
+
+post_process :: (ListEntry -> Maybe ListEntry) -> ListEntry -> Maybe ListEntry
+post_process func e@(File _) = func e
+post_process func (Directory path children) = func $ Directory path $ removeNothings $ fmap (post_process func) children
+
--- Main.hs
+++ Main.hs
@@ -1,8 +1,8 @@
 module Main(main) where
 
-import Lister (build_list, filter_files)
+import Lister (build_list, collapse_dirs)
 import Mime (get_videos)
 import System.Environment (getArgs)
 import Control.Monad ((<=<), join)
 
-main = mapM_ (print . join . fmap get_videos <=< build_list) =<< getArgs 
+main = mapM_ (print . join . fmap (collapse_dirs <=< get_videos) <=< build_list) =<< getArgs 
