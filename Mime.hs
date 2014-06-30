4681aea384cd55f31755e962589c40867f2b468c
Christopher Vollick <0@psycoti.ca>
2014-06-30 08:38:50 -0400

    Used Network.Mime

--- Mime.hs
+++ Mime.hs
@@ -1,16 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Mime (get_videos) where
 
---import Network.Mime (defaultMimeLookup)
+import Network.Mime (defaultMimeLookup)
 import Lister (ListEntry(..), filter_files)
 import Data.ByteString.Char8 (isPrefixOf, ByteString)
 import Data.List (isSuffixOf)
+import Data.Text (pack)
 
 get_videos :: ListEntry -> Maybe ListEntry
 get_videos = filter_files is_video
 	where
-	is_video = isPrefixOf "video/" . defaultMimeLookup
-
-defaultMimeLookup :: FilePath -> ByteString
-defaultMimeLookup path | (".mkv" `isSuffixOf` path) = "video/mkv"
-defaultMimeLookup _ = "text/plain"
+	is_video = isPrefixOf "video/" . defaultMimeLookup . pack
