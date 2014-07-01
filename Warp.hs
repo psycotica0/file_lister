3e1e5a28dad54e0b501ce340dbcfa4c57cd747b8
Christopher Vollick <0@psycoti.ca>
2014-07-01 14:35:12 -0400

    Better Error Handling
    Server refuses to start if you don't give it at least one dir.

--- Warp.hs
+++ Warp.hs
@@ -3,5 +3,9 @@ module Main(main) where
 import Network.Wai.Handler.Warp (run)
 import System.Environment (getArgs)
 import Application (app)
+import Control.Monad (unless)
 
-main = run 8080 . app =<< getArgs
+main = do
+	args <- getArgs
+	unless (length args > 0) $ fail "Must Provide at Least One Directory"
+	run 8080 $ app args
