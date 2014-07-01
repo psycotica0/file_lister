c9e872da4a4472bef4ce972cdd307e77e5a1dab5
Christopher Vollick <0@psycoti.ca>
2014-07-01 10:27:32 -0400

    Initial Directory Listing

--- Application.hs
+++ Application.hs
@@ -5,6 +5,7 @@ import Lister(build_list, collapse_dirs)
 import Mime(get_videos)
 import Network.Wai (Application, responseLBS)
 import Network.HTTP.Types.Status(ok200)
+import Data.ByteString.Lazy.Char8(pack)
 
 app :: Application
-app req cont = cont $ responseLBS ok200 [] "Hello Fucker!"
+app req cont = (cont . responseLBS ok200 [] . pack . show) =<< (fmap (fmap collapse_dirs) $ build_list "test")
