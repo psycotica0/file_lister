1277d2382b3ad203e0d5ce8724fca7998ca8052a
Christopher Vollick <0@psycoti.ca>
2014-07-01 14:45:08 -0400

    Sort For Display

--- Application.hs
+++ Application.hs
@@ -10,6 +10,7 @@ import Blaze.ByteString.Builder.ByteString(fromLazyByteString)
 import Data.Monoid ((<>), mconcat)
 import Data.ByteString.Lazy.Char8 (pack)
 import Control.Monad (join, (<=<))
+import Data.List (sort)
 
 html_template :: Builder -> Builder
 html_template body = fromLazyByteString "<!DOCTYPE html><html><head><title>File Listing</title></head><body>" <> body <> fromLazyByteString "</body></html>"
@@ -26,6 +27,10 @@ empty_output = html_template $ fromLazyByteString "<h1>No Files Found</h1>"
 filters :: Maybe ListEntry -> Maybe ListEntry
 filters = join . fmap (collapse_dirs <=< get_videos)
 
+sort_entries :: ListEntry -> ListEntry
+sort_entries f@(File _) = f
+sort_entries (Directory p children) = Directory p $ sort children
+
 maybe_merge :: Maybe ListEntry -> Maybe ListEntry -> Maybe ListEntry
 maybe_merge Nothing a = a
 maybe_merge a Nothing = a
@@ -34,4 +39,4 @@ maybe_merge (Just a) (Just b) = Just $ merge a b
 app :: [FilePath] -> Application
 app roots req cont = do
 	merged <- fmap (foldr1 maybe_merge) $ mapM build_list roots
-	cont $ responseBuilder ok200 [] $ maybe empty_output html_output $ filters merged
+	cont $ responseBuilder ok200 [] $ maybe empty_output html_output $ fmap sort_entries $ filters merged
