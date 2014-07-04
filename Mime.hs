46c9ea8532483fd0648ae7db2456972166d2582e
Christopher Vollick <0@psycoti.ca>
2014-07-03 22:36:46 -0400

    Ensure My Files are Shown
    I've made sure the file types I have are in the list...

--- Mime.hs
+++ Mime.hs
@@ -1,13 +1,27 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Mime (get_videos) where
 
-import Network.Mime (defaultMimeLookup)
+import Network.Mime (defaultMimeType, mimeByExt, defaultMimeMap)
 import Lister (ListEntry(..), filter_files)
 import Data.ByteString.Char8 (isPrefixOf, ByteString)
 import Data.List (isSuffixOf)
 import Data.Text (pack)
+import qualified Data.Map as Map (fromAscList, union)
 
 get_videos :: ListEntry -> Maybe ListEntry
 get_videos = filter_files is_video
 	where
-	is_video = isPrefixOf "video/" . defaultMimeLookup . pack
+	is_video = isPrefixOf "video/" . mimeByExt mimeMap defaultMimeType . pack
+
+mimeMap = defaultMimeMap `Map.union` Map.fromAscList [
+	("3gp", "video/3gpp")
+	, ("avi", "video/x-msvideo")
+	, ("divx", "video/x-msvideo")
+	, ("flv", "video/x-flv")
+	, ("m4v", "video/x-m4v")
+	, ("mkv", "video/x-matroska")
+	, ("mov", "video/quicktime")
+	, ("mp4", "video/mp4")
+	, ("mpg", "video/mpeg")
+	, ("wmv", "video/x-ms-wmv")
+	]
