f206ec68727777ae96b9c8d251bca5caa4df2ad9
Christopher Vollick <0@psycoti.ca>
2014-07-05 07:32:44 -0400

    Print Message Before Newline
    The whole point of the message is that it tells you what it's doing while it's
    doing it.
    
    It was line buffering, and so it "printed" the message, but didn't flush the
    buffer into the terminal until it was done and printed the second part.
    Not useful.

--- Warp.hs
+++ Warp.hs
@@ -3,12 +3,14 @@ module Main(main) where
 import Network.Wai.Handler.Warp (run)
 import System.Environment (getArgs)
 import Application (preloaded_app, preload_list)
+import System.IO (hFlush, stdout)
 import Control.Monad (unless)
 
 main = do
 	args <- getArgs
 	unless (length args > 0) $ fail "Must Provide at Least One Directory"
 	putStr "Reading File List..."
+	hFlush stdout
 	list <- preload_list args
 	putStrLn " Done"
 	putStrLn "Listening on 8080"
